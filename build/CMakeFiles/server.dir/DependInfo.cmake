29fb3725ca61773ce039bd0b9c5ac5dae0704b31
qmhntp9 <qmhntp9@users.noreply.github.com>
2026-08-20 18:11:55 +0000

    feat: Cmake 项目确认
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

---
+++ build/CMakeFiles/server.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/workspace/CGImysql/sql_connection_pool.cpp" "CMakeFiles/server.dir/CGImysql/sql_connection_pool.cpp.o" "gcc" "CMakeFiles/server.dir/CGImysql/sql_connection_pool.cpp.o.d"
+  "/workspace/config.cpp" "CMakeFiles/server.dir/config.cpp.o" "gcc" "CMakeFiles/server.dir/config.cpp.o.d"
+  "/workspace/http/http_conn.cpp" "CMakeFiles/server.dir/http/http_conn.cpp.o" "gcc" "CMakeFiles/server.dir/http/http_conn.cpp.o.d"
+  "/workspace/log/log.cpp" "CMakeFiles/server.dir/log/log.cpp.o" "gcc" "CMakeFiles/server.dir/log/log.cpp.o.d"
+  "/workspace/main.cpp" "CMakeFiles/server.dir/main.cpp.o" "gcc" "CMakeFiles/server.dir/main.cpp.o.d"
+  "/workspace/timer/lst_timer.cpp" "CMakeFiles/server.dir/timer/lst_timer.cpp.o" "gcc" "CMakeFiles/server.dir/timer/lst_timer.cpp.o.d"
+  "/workspace/webserver.cpp" "CMakeFiles/server.dir/webserver.cpp.o" "gcc" "CMakeFiles/server.dir/webserver.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
